3829f33921c86ee97282a776ccf7887f11e50206
Sunburst275 <Timo.Vandrey@haw-hamburg.de>
2021-03-14 23:21:01 +0100

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,197 @@
+﻿
+namespace PasswordManager
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MainMenu = new System.Windows.Forms.MenuStrip();
+            this.File = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuFileLoad = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuFileSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuFileSaveAs = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuFileSep1 = new System.Windows.Forms.ToolStripSeparator();
+            this.MenuFileExport = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuFileSep2 = new System.Windows.Forms.ToolStripSeparator();
+            this.MenuFileExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.Edit = new System.Windows.Forms.ToolStripMenuItem();
+            this.Help = new System.Windows.Forms.ToolStripMenuItem();
+            this.MainSplitContainer = new System.Windows.Forms.SplitContainer();
+            this.DataGridView = new System.Windows.Forms.DataGridView();
+            this.MainMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.MainSplitContainer)).BeginInit();
+            this.MainSplitContainer.Panel2.SuspendLayout();
+            this.MainSplitContainer.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MainMenu
+            // 
+            this.MainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.File,
+            this.Edit,
+            this.Help});
+            this.MainMenu.Location = new System.Drawing.Point(0, 0);
+            this.MainMenu.Name = "MainMenu";
+            this.MainMenu.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.MainMenu.Size = new System.Drawing.Size(800, 24);
+            this.MainMenu.TabIndex = 0;
+            this.MainMenu.Text = "MainMenu";
+            // 
+            // File
+            // 
+            this.File.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuFileLoad,
+            this.MenuFileSave,
+            this.MenuFileSaveAs,
+            this.MenuFileSep1,
+            this.MenuFileExport,
+            this.MenuFileSep2,
+            this.MenuFileExit});
+            this.File.Name = "File";
+            this.File.Size = new System.Drawing.Size(37, 20);
+            this.File.Text = "File";
+            // 
+            // MenuFileLoad
+            // 
+            this.MenuFileLoad.Name = "MenuFileLoad";
+            this.MenuFileLoad.Size = new System.Drawing.Size(180, 22);
+            this.MenuFileLoad.Text = "Load";
+            // 
+            // MenuFileSave
+            // 
+            this.MenuFileSave.Name = "MenuFileSave";
+            this.MenuFileSave.Size = new System.Drawing.Size(180, 22);
+            this.MenuFileSave.Text = "Save";
+            // 
+            // MenuFileSaveAs
+            // 
+            this.MenuFileSaveAs.Name = "MenuFileSaveAs";
+            this.MenuFileSaveAs.Size = new System.Drawing.Size(180, 22);
+            this.MenuFileSaveAs.Text = "Save as ...";
+            // 
+            // MenuFileSep1
+            // 
+            this.MenuFileSep1.Name = "MenuFileSep1";
+            this.MenuFileSep1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // MenuFileExport
+            // 
+            this.MenuFileExport.Name = "MenuFileExport";
+            this.MenuFileExport.Size = new System.Drawing.Size(180, 22);
+            this.MenuFileExport.Text = "Export ...";
+            // 
+            // MenuFileSep2
+            // 
+            this.MenuFileSep2.Name = "MenuFileSep2";
+            this.MenuFileSep2.Size = new System.Drawing.Size(177, 6);
+            // 
+            // MenuFileExit
+            // 
+            this.MenuFileExit.Name = "MenuFileExit";
+            this.MenuFileExit.Size = new System.Drawing.Size(180, 22);
+            this.MenuFileExit.Text = "Exit";
+            // 
+            // Edit
+            // 
+            this.Edit.Name = "Edit";
+            this.Edit.Size = new System.Drawing.Size(39, 20);
+            this.Edit.Text = "Edit";
+            // 
+            // Help
+            // 
+            this.Help.Name = "Help";
+            this.Help.Size = new System.Drawing.Size(44, 20);
+            this.Help.Text = "Help";
+            // 
+            // MainSplitContainer
+            // 
+            this.MainSplitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainSplitContainer.Location = new System.Drawing.Point(0, 24);
+            this.MainSplitContainer.Name = "MainSplitContainer";
+            // 
+            // MainSplitContainer.Panel2
+            // 
+            this.MainSplitContainer.Panel2.Controls.Add(this.DataGridView);
+            this.MainSplitContainer.Size = new System.Drawing.Size(800, 426);
+            this.MainSplitContainer.SplitterDistance = 266;
+            this.MainSplitContainer.TabIndex = 1;
+            // 
+            // DataGridView
+            // 
+            this.DataGridView.AllowUserToAddRows = false;
+            this.DataGridView.AllowUserToDeleteRows = false;
+            this.DataGridView.AllowUserToOrderColumns = true;
+            this.DataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.DataGridView.Location = new System.Drawing.Point(0, 0);
+            this.DataGridView.MultiSelect = false;
+            this.DataGridView.Name = "DataGridView";
+            this.DataGridView.ReadOnly = true;
+            this.DataGridView.ShowEditingIcon = false;
+            this.DataGridView.Size = new System.Drawing.Size(530, 426);
+            this.DataGridView.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.MainSplitContainer);
+            this.Controls.Add(this.MainMenu);
+            this.MainMenuStrip = this.MainMenu;
+            this.Name = "MainForm";
+            this.Text = "Password Manager";
+            this.MainMenu.ResumeLayout(false);
+            this.MainMenu.PerformLayout();
+            this.MainSplitContainer.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.MainSplitContainer)).EndInit();
+            this.MainSplitContainer.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip MainMenu;
+        private System.Windows.Forms.ToolStripMenuItem File;
+        private System.Windows.Forms.ToolStripMenuItem Edit;
+        private System.Windows.Forms.ToolStripMenuItem Help;
+        private System.Windows.Forms.ToolStripMenuItem MenuFileLoad;
+        private System.Windows.Forms.ToolStripMenuItem MenuFileSave;
+        private System.Windows.Forms.ToolStripMenuItem MenuFileSaveAs;
+        private System.Windows.Forms.ToolStripMenuItem MenuFileExit;
+        private System.Windows.Forms.ToolStripSeparator MenuFileSep2;
+        private System.Windows.Forms.SplitContainer MainSplitContainer;
+        private System.Windows.Forms.DataGridView DataGridView;
+        private System.Windows.Forms.ToolStripSeparator MenuFileSep1;
+        private System.Windows.Forms.ToolStripMenuItem MenuFileExport;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PasswordManager
+{
+
+    // TODO: DataGridView als readonly machen.
+    // TODO: Das Panel1 vom SplitContainer ist dafuer da, dass man da die Daten, die man Anwaehlt, aendern und bearbeiten kann.
+    // TODO: DataStorage erstellen
+
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
